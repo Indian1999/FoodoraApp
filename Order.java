e984bed3e3c0eb2e5bc785391a88ac55d9bf7336
Indian1999 <haszab99@gmail.com>
2023-11-27 05:28:26 +0100

    formatSettingsCommit

--- Order.java
+++ Order.java
@@ -1,90 +1,110 @@
-import java.util.List;
 import java.time.LocalDateTime;
+import java.util.List;
 
-class Order {
+class Order
+{
     private Customer customer;
     private Restaurant restaurant;
     private Courier courier;
     private List<Item> items;
 
-    private enum status {
-        PLACED, READY, BEING_DELIVERED, DELIVERED
+    private enum status
+    {
+        PLACED,
+        READY,
+        BEING_DELIVERED,
+        DELIVERED
     }
 
     private LocalDateTime orderPlace;
-
     private LocalDateTime orderReady;
     private LocalDateTime orderDeliveryPickup;
     private LocalDateTime orderDelivered;
 
-    Order(Customer c, Restaurant r, List<Item> items) {
+    Order(Customer c, Restaurant r, List<Item> items)
+    {
         this.customer = c;
         this.restaurant = r;
         this.items = items;
     }
 
-    public Customer getCustomer() {
+    public Customer getCustomer()
+    {
         return customer;
     }
 
-    public void setCustomer(Customer c) {
+    public void setCustomer(Customer c)
+    {
         customer = c;
     }
 
-    public Restaurant getRestaurant() {
+    public Restaurant getRestaurant()
+    {
         return restaurant;
     }
 
-    public void setRestaurant(Restaurant r) {
+    public void setRestaurant(Restaurant r)
+    {
         restaurant = r;
     }
 
-    public Courier getCourier() {
+    public Courier getCourier()
+    {
         return courier;
     }
 
-    public void setCourier(Courier c) {
+    public void setCourier(Courier c)
+    {
         courier = c;
     }
 
-    public List<Item> getItems() {
+    public List<Item> getItems()
+    {
         return items;
     }
 
-    public void setItems(List<Item> items) {
+    public void setItems(List<Item> items)
+    {
         this.items = items;
     }
 
-    public LocalDateTime getOrderPlace() {
+    public LocalDateTime getOrderPlace()
+    {
         return orderPlace;
     }
 
-    public void setOrderPlace(LocalDateTime orderPlace) {
+    public void setOrderPlace(LocalDateTime orderPlace)
+    {
         this.orderPlace = orderPlace;
     }
 
-    public LocalDateTime getOrderReady() {
+    public LocalDateTime getOrderReady()
+    {
         return orderReady;
     }
 
-    public void setOrderReady(LocalDateTime orderReady) {
+    public void setOrderReady(LocalDateTime orderReady)
+    {
         this.orderReady = orderReady;
     }
 
-    public LocalDateTime getOrderDeliveryPickup() {
+    public LocalDateTime getOrderDeliveryPickup()
+    {
         return orderDeliveryPickup;
     }
 
-    public void setOrderDeliveryPickup(LocalDateTime orderDeliveryPickup) {
+    public void setOrderDeliveryPickup(LocalDateTime orderDeliveryPickup)
+    {
         this.orderDeliveryPickup = orderDeliveryPickup;
     }
 
-    public LocalDateTime getOrderDelivered() {
+    public LocalDateTime getOrderDelivered()
+    {
         return orderDelivered;
     }
 
-    public void setOrderDelivered(LocalDateTime orderDelivered) {
+    public void setOrderDelivered(LocalDateTime orderDelivered)
+    {
         this.orderDelivered = orderDelivered;
     }
-
 }
